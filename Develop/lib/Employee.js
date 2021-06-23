23a3ccf790e5528d08d1f8245291141ae4c4e9cf
Tuyet Nguyen <tuyetbqcba55@gmail.com>
2021-06-22 23:37:13 -0500

    edited

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,26 @@
+
+// Employee is parent class
+class Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+
+    getName() {
+     return this.name;
+    }
+    getId() {
+        return this.id;
+       }
+    getEmail(){
+        return this.email;
+        }
+
+    getRole() {
+        return "Employee";
+    }
+
+  }
+
+  module.exports = Employee;
